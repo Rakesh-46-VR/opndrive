c0bc84931417f35321c6b29758e6f0a6522d0f7f
rakesh b <rakeshbsirvi46@gmail.com>
2025-07-28 00:47:19 +0530

    feat: initialized supabase types

--- backend/src/utils/supabase-client.ts
+++ backend/src/utils/supabase-client.ts
@@ -1,3 +1,4 @@
+import { Database } from '@/types/supabase';
 import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
 
@@ -5,7 +6,7 @@ dotenv.config();
 const supabaseUrl = process.env.PUBLIC_SUPABASE_URL!;
 const supabaseServiceRoleKey = process.env.PRIVATE_SUPABASE_SERVICE_ROLE_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseServiceRoleKey, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseServiceRoleKey, {
   auth: {
     persistSession: true,
   },
